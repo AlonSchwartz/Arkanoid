af30fd8066eabbab0c34cd00b83c0764944904db
elchanan <eli032.eb@gmail.com>
2018-12-05 19:29:46 +0200

    Code refactor: bricks disappear , messages works. TODO: Play with collision logic, sound, design, long touch..

--- app/src/main/java/com/elchananalon/arkanoid/GameView.java
+++ app/src/main/java/com/elchananalon/arkanoid/GameView.java
@@ -18,17 +18,10 @@ public class GameView extends View {
     private int canvasHeight;
     private int countLives, countScore;
 
-
-    private Paddle paddle = new Paddle(180 ,40);
-    //private Brick brick = new Brick(180,50,20,50, Color.RED);
-    private Brick brick2 = new Brick(180,50,720,350, Color.RED);
-    //private Brick brick3 = new Brick(180,50,180+20+10,110, Color.RED);
-    //private Brick brick4 = new Brick(180,50,210+180+10,110, Color.RED);
-    private Brick brick5 = new Brick(180,50,380,395, Color.RED);
-    private BrickCollection bricks = new BrickCollection(180,50,20,105, 20, Color.RED);
-
+    private Paddle paddle;
+    private BrickCollection bricks;
     private float fx, fy;       // for finger touch location
-    private Paint penMsg , penInfo;
+    private Paint penMsg , penInfo;  // messages on screen
 
     // current state
     private State state;
@@ -38,22 +31,19 @@ public class GameView extends View {
 
     }
     private void initGame(){
+        // init score and lives
         countLives = 3;
         countScore = 0;
-        state = State.GET_READY;
-        // initialize ball
-        movingBall = new Ball(canvasWidth / 2.0f, canvasHeight-80, 50);
-        movingBall.setDx(-7);
-        movingBall.setDy(5);
-        // Initialize paddle
-        paddle = new Paddle(180 ,40);
-        paddle.setXPosition(canvasWidth / 2.0f);
-        paddle.setYPosition(canvasHeight-45);
+
+        initCanvas();
+        // init brick collection
+        bricks = new BrickCollection(180,50,20,105, 20, Color.RED);
 
         // paint for info text
         penInfo = new Paint(Paint.ANTI_ALIAS_FLAG);
         penInfo.setColor(Color.YELLOW);
         penInfo.setTextSize(45);
+
         // paint for messages text
         penMsg = new Paint(Paint.ANTI_ALIAS_FLAG);
         penMsg.setTextAlign(Paint.Align.CENTER);
@@ -63,28 +53,33 @@ public class GameView extends View {
         penMsg.setTextSize(55);
 
     }
+
+    private void initCanvas()
+    {
+        state = State.GET_READY;
+        // initialize ball
+        movingBall = new Ball(canvasWidth / 2.0f, canvasHeight-80, 50);
+        movingBall.setDx(-7);
+        movingBall.setDy(5);
+        // Initialize paddle
+        paddle = new Paddle(180 ,40);
+        paddle.setXPosition(canvasWidth / 2.0f);
+        paddle.setYPosition(canvasHeight-45);
+    }
+
     @Override
     protected void onDraw(Canvas canvas){
         super.onDraw(canvas);
+        // draw objects on canvas
         movingBall.draw(canvas);
         paddle.draw(canvas);
-       //brick.draw(canvas);
-      // brick2.draw(canvas);
-       // brick3.draw(canvas);
-       // brick5.draw(canvas);
-        //paddle.move(1180,768,1);
         bricks.draw(canvas);
         penInfo.setTextAlign(Paint.Align.RIGHT);
         canvas.drawText("Lives: " + countLives, canvasWidth-50, 100, penInfo);
         penInfo.setTextAlign(Paint.Align.LEFT);
         canvas.drawText("Score: " + countScore, 50, 100, penInfo);
-        for(int i =0 ; i<bricks.getBricks().size();i++) {
-            if (movingBall.collideWith(bricks.getBricks().get(i))) {
-                movingBall.setDx(-movingBall.getDx());
-                movingBall.setDy(-movingBall.getDy());
-            }
-        }
-        movingBall.collideWith(paddle);
+
+
 
         switch (state)
         {
@@ -96,12 +91,14 @@ public class GameView extends View {
 
                 // move the movingBall
                 movingBall.move(canvasWidth, canvasHeight);
-
                 // check collision
                 movingBall.collideWith(paddle);
                 for(int i =0 ; i<bricks.getBricks().size();i++){
                     if (movingBall.collideWith(bricks.getBricks().get(i))) {
-                        System.out.println("HIT!");
+                        // the following logic should be adjusted
+                        movingBall.setDx(-movingBall.getDx());
+                        movingBall.setDy(-movingBall.getDy());
+                        bricks.remove(i);
                         countScore += 5;
                     }
                 }
@@ -110,11 +107,21 @@ public class GameView extends View {
                     if(countLives == 0){
                         state = State.GAME_OVER;
                     }
+                    else{
+                        initCanvas();
+                    }
                 }
                 break;
 
             case GAME_OVER:
-                canvas.drawText("GAME OVER - You Loss!", canvasWidth/2, canvasHeight/2, penMsg);
+                if(bricks.getBricks().size() == 0){//WINN
+                    canvas.drawText("WELL DONE! - You Win ", canvasWidth/2, canvasHeight/2, penMsg);
+                    canvas.drawText(" please touch the screen to start new game", canvasWidth/2, canvasHeight/2 + penMsg.getTextSize()+5, penMsg);
+                }
+                else{//loss
+                    canvas.drawText("GAME OVER - You Loss!", canvasWidth/2, canvasHeight/2, penMsg);
+                }
+
                 break;
         }
         invalidate();
@@ -127,16 +134,17 @@ public class GameView extends View {
         fx = event.getX();
         fy = event.getY();
 
-        switch (event.getAction()){
-
-            case MotionEvent.ACTION_DOWN:
-
+        if (event.getAction() == MotionEvent.ACTION_DOWN){
                 if(state == State.GET_READY)
                     state = State.PLAYING;
                 else
                 {
                     if(state == State.PLAYING)
                     {
+                        if(fx > canvasWidth/2 )
+                            paddle.move(canvasWidth,canvasHeight,0);
+                        else if(fx <= canvasWidth/2)
+                            paddle.move(canvasWidth,canvasHeight,1);
 
                     }
                     else
@@ -144,21 +152,7 @@ public class GameView extends View {
                         initGame();
                     }
                 }
-
-                break;
-
-            case MotionEvent.ACTION_MOVE:
-                //?? do we need it ??
-                break;
-
-            case MotionEvent.ACTION_UP:
-                if(fx > canvasWidth/2 )
-                    paddle.move(canvasWidth,canvasHeight,0);
-                else if(fx <= canvasWidth/2)
-                    paddle.move(canvasWidth,canvasHeight,1);
-                break;
         }
-
         invalidate();
 
         return true;
